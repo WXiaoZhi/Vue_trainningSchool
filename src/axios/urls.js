bcc5d6696aec3ebabcdcf55998d455d64474f619
张佳翼 <uiversestar@qq.com>
2023-05-30 21:16:00 +0800

    0.2_我的应用界面实现删除添加应用

--- src/axios/urls.js
+++ src/axios/urls.js
@@ -1,7 +1,8 @@
 const  urls={
     BASE_URL                         :"http://localhost:8080/",
-    MY_APPS_URL                      :"/myapps.json",
-    ALL_APPS_URL                     :"/allapps.json"
+    MY_APPS_URL                      :"/data/myapps.json",
+    ALL_APPS_URL                     :"/data/allapps.json",
+    NOTICE_URL                       :"/data/notices.json"
 }
 
 export  default urls
---
+++ src/components/HomeRight.vue
@@ -0,0 +1,144 @@
+<template>
+    <div>
+        <el-row>
+            <el-col :span="12">
+                <div class="con3_1"></div>
+            </el-col>
+            <el-col :span="12">
+                <div class="con3_2">
+                    <div class="con4">
+                        <div class="con4_1">通知</div>
+                        <div class="con4_2">更多</div>
+                    </div>
+                    <template>
+                        <el-table
+                                :data="notices"
+                                :show-header="false"
+                                style="width: 100%">
+                            <el-table-column
+                                    prop="title"
+                                    width="385"
+                                   >
+                            </el-table-column>
+
+                            <el-table-column
+                                    prop="publish-data"
+                                    width="100">
+                            </el-table-column>
+                        </el-table>
+                    </template>
+                </div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">
+                <div class="con3_3"></div>
+            </el-col>
+            <el-col :span="12">
+                <div class="con3_4"></div>
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="12">
+                <div class="con3_5"></div>
+            </el-col>
+            <el-col :span="12">
+                <div class="con3_6"></div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "HomeRight",
+    data() {
+        return {
+            notices: []
+        }
+    },
+    created() {
+        this.getNotices()
+    },
+    methods: {
+        getNotices() {
+            var that = this
+            this.$axios({
+                method: "get",
+                url: this.$urls.NOTICE_URL
+            }).then(function (res) {
+                that.notices = res.data
+            })
+        }
+    }
+
+}
+</script>
+
+<style scoped="scoped">
+
+.el-row {
+    margin-bottom: 5px;
+}
+
+.el-col {
+    /*margin-left: 5px;*/
+    /*background-color: #FFFFFF;*/
+    padding-left: 5px;
+}
+
+.con3_1 {
+    height: 200px;
+    background-color: #FFFFFF;
+    /*margin-left: 5px;*/
+    /*margin-top: 5px;*/
+}
+
+.con3_2 {
+    height: 200px;
+    background-color: #FFFFFF;
+    font-size: 14px;
+}
+
+.con3_3 {
+    height: 200px;
+    background-color: #FFFFFF;
+}
+
+.con3_4 {
+    height: 200px;
+    background-color: #FFFFFF;
+}
+
+.con3_5 {
+    height: 200px;
+    background-color: #FFFFFF;
+}
+
+.con3_6 {
+    height: 200px;
+    background-color: #FFFFFF;
+}
+.con4{
+    /*margin-top: 5px;*/
+    padding: 5px;
+    display: flex;
+    justify-content: space-between;
+}
+.con4_1{
+    font-size: 14px;
+    font-weight: 600;
+}
+.con4_2{
+    font-size: 14px;
+    font-weight: 600;
+    text-decoration: underline;
+    color: #336699;
+}
+.el-table-column{
+    width:380px;
+    white-space:nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+</style>
